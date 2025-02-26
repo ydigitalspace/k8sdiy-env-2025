6b1af3b27cbda800614a10ee5160d1839a68e079
Borodaty <31692425+Borodaty@users.noreply.github.com>
2025-02-26 17:13:55 +0000

    feat: use $SHELL variable to avoid errors related to shell

--- codespace.sh
+++ codespace.sh
@@ -8,13 +8,15 @@
 ## hashicorp/tap/terraform opentofu 
 # yes|brew install tenv fluxcd/tap/flux htop kind 
 
+
+
 # Install OpenTofu
 curl -fsSL https://get.opentofu.org/install-opentofu.sh | sh -s -- --install-method standalone 
 
 # Install K9S to manage the cluster
 curl -sS https://webi.sh/k9s | sh
-curl -s https://fluxcd.io/install.sh | sh  ## To avoid errors, for bash terminal you may update this line to: curl -s https://fluxcd.io/install.sh | bash 
-. <(flux completion zsh)   ## To avoid errors, for bash terminal you may update this line to:  . <(flux completion bash)
+curl -s https://fluxcd.io/install.sh | $(basename "$SHELL")  ## To avoid errors, for bash terminal you may update this line to: curl -s https://fluxcd.io/install.sh | bash 
+. <(flux completion $(basename "$SHELL"))   ## To avoid errors, for bash terminal you may update this line to:  . <(flux completion bash)
 
 # Initialize Tofu
 cd bootstrap
