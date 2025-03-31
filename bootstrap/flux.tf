0322f8bb18027fa6e6271a063484ad5cc00f1b3e
Denys Vasyliev <den.vasyliev@gmail.com>
2025-03-31 18:46:11 +0000

    tf fmt

--- bootstrap/flux.tf
+++ bootstrap/flux.tf
@@ -3,7 +3,7 @@
 # ==========================================
 resource "helm_release" "flux_operator" {
   depends_on = [kind_cluster.this]
-  
+
   name             = "flux-operator"
   namespace        = "flux-system"
   repository       = "oci://ghcr.io/controlplaneio-fluxcd/charts"
